b195463f3ef28a4e8d26df2a35c6793f982682ba
KNicole <38897127+Katynicole89@users.noreply.github.com>
2021-08-14 21:45:23 -0500

    Updating

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,7 +1,22 @@
+var languageButtonsEL = document.querySelector("#language-buttons");
 var userFormEl = document.querySelector("#user-form");
 var nameInputEl = document.querySelector("#username");
 var repoContainerEl = document.querySelector("#repos-container");
 var repoSearchTerm = document.querySelector("#repo-search-term");
+
+var getFeaturedRepos = function(language) {
+  var apiUrl = "https://api.github.com/search/repositories?q=" + language + "+is:featured&sort=help-wanted-issues";
+
+  fetch(apiUrl).then(function(response) {
+    if (response.ok) {
+      response.json().then(function(data) {
+        console.log(data)
+      });
+    } else {
+      alert('Error: GitHub User Not Found');
+    }
+  });
+};
 
 var formSubmitHandler = function(event) {
   // prevent page from refreshing
@@ -90,5 +105,17 @@ var displayRepos = function(repos, searchTerm) {
   }
 };
 
+var buttonClickHandler = function(event){
+  var language = event.target.getAttribute("data-language");
+  
+  if (language){
+    getFeaturedRepos(language);
+
+    //clear old content
+    repoContainerEl.textContent = "";
+  }
+}
+
 // add event listeners to forms
 userFormEl.addEventListener("submit", formSubmitHandler);
+languageButtonsEL.addEventListener("click", buttonClickHandler);
